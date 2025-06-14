b9bcb7fcbaabdfc68d87ce8c1a2248a50f6952e6
Elisabete <msr_hck1@hotmail.com>
2025-06-14 12:21:25 -0300

    made a simple window

--- gui.py
+++ gui.py
@@ -1,2 +1,11 @@
+import tkinter as tk
+
+def launch_gui(bg_color):
+    root = tk.Tk()
+    root.title("My GUI test")
+    root.configure(bg=bg_color)
+    root.geometry("400x200")
+    root.mainloop()
+
 def greet():
     print("Hello Venus\n")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from logic import greet as logic_greet
 from gui import greet as gui_greet
+from gui import launch_gui
 
 def main():
     print("Hello earth!\n")
     logic_greet()
     gui_greet()
+    launch_gui("#A0D8EF")
 
 if __name__=="__main__":
     main()
